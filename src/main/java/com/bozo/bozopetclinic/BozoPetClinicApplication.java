3c4194449692f16e6d333bf850bc220d315e8f37
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-08-26 20:55:13 +0200

    Initial commit

---
+++ src/main/java/com/bozo/bozopetclinic/BozoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.bozo.bozopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BozoPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BozoPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bozo/bozopetclinic/BozoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bozo.bozopetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BozoPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

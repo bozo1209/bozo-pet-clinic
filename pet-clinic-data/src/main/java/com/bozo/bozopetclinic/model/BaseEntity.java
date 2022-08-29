2810ca6bead8b0d5ebbd8361ea3125f9f16fbd18
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-08-29 22:51:09 +0200

    adding base entity. Closes #11

--- pet-clinic-data/src/main/java/com/bozo/bozopetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/bozo/bozopetclinic/model/BaseEntity.java
@@ -3,9 +3,11 @@ package com.bozo.bozopetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class PetType extends BaseEntity{
+public class BaseEntity implements Serializable {
 
-    private String name;
+    private Long id;
 }

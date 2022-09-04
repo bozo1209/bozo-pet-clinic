c958d57460ab562ab6d17bb3aa8a6f746ebaa065
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-09-04 23:58:04 +0200

    create vet index page and controller. Closes #6

--- pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/VetController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class VetController {
 
-    @GetMapping(path = {"", "/", "index", "index.html"})
-    public String index(){
-        return "index";
+    @GetMapping(path = {"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
     }
 }

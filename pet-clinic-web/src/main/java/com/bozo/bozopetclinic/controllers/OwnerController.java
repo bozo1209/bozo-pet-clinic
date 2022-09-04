34d24a620e2e26175697abecb6e0dc36ca05da3a
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-09-05 00:11:09 +0200

    create owner index page and controller. Closes #19

--- pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/OwnerController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(path = "/vets")
+@RequestMapping(path = "/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @GetMapping(path = {"", "/", "/index", "/index.html"})
-    public String listVets(){
-        return "vets/index";
+    public String listOwners(){
+        return "owners/index";
     }
 }

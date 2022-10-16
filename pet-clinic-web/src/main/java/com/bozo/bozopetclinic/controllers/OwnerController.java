0cf52f053ee8fd971c01e7b333c20e6fb2cb5662
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-10-16 20:25:14 +0200

    list all owners on owners index page. Closes #22

--- pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.bozo.bozopetclinic.controllers;
 
+import com.bozo.bozopetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping(path = {"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }

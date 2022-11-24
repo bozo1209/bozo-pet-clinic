e7ef9175d88425671575d1f3b18eb676cbac56fc
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-11-24 20:39:58 +0100

    menu links are broken. Closes #72

--- pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/OwnerController.java
@@ -21,4 +21,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+    @GetMapping(path = "/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
 }
--- pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/VetController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(path = "/vets")
+@RequestMapping(path = {"/vets", "/vets.html"})
 @Controller
 public class VetController {
 

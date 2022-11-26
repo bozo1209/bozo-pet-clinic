f06f5dd1dd305313a76290851b1a4137e8334b59
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-11-26 18:44:57 +0100

    add spring data jpa repositories. Closes #59

---
+++ pet-clinic-data/src/main/java/com/bozo/bozopetclinic/repository/SpecialityRepository.java
@@ -0,0 +1,7 @@
+package com.bozo.bozopetclinic.repository;
+
+import com.bozo.bozopetclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/bozo/bozopetclinic/repository/VetRepository.java
@@ -0,0 +1,7 @@
+package com.bozo.bozopetclinic.repository;
+
+import com.bozo.bozopetclinic.model.Vet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VetRepository extends CrudRepository<Vet, Long> {
+}

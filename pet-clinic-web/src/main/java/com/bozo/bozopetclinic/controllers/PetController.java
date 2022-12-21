17722f28b06d7b923d64f5147e79b799cbf2c589
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-12-21 20:52:34 +0100

    add ability to add or update a pet. #93

---
+++ pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/PetController.java
@@ -0,0 +1,45 @@
+package com.bozo.bozopetclinic.controllers;
+
+import com.bozo.bozopetclinic.model.Owner;
+import com.bozo.bozopetclinic.model.PetType;
+import com.bozo.bozopetclinic.service.OwnerService;
+import com.bozo.bozopetclinic.service.PetService;
+import com.bozo.bozopetclinic.service.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping(path = "/owners/{ownerId}")
+public class PetController {
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes(){
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId){
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void innitOwnerBinder(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+}

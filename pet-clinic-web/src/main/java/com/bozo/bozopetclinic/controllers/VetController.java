5873f12ae33f4a079bb9a7dac203d6f67574db6c
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-12-30 18:23:56 +0100

    implement vets json endpoint. #159

--- pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/bozo/bozopetclinic/controllers/VetController.java
@@ -1,22 +1,30 @@
 package com.bozo.bozopetclinic.controllers;
 
+import com.bozo.bozopetclinic.model.Vet;
 import com.bozo.bozopetclinic.service.VetService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping(path = {"/vets", "/vets.html"})
+import java.util.Set;
+
 @Controller
 @AllArgsConstructor
 public class VetController {
 
     private final VetService vetService;
 
-    @GetMapping(path = {"", "/", "/index", "/index.html"})
+    @GetMapping(path = {"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping(path = "/api/vets")
+    @ResponseBody
+    public Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
